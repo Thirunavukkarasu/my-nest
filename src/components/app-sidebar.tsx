08af1554063cbc8660fe08943c747fbd4696e96d
thirunavukkarasu <thirunavukkarasu.sam@gmail.com>
2024-11-23 22:07:59 +0530

    add base layout

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import * as React from "react";
+import { GalleryVerticalEnd, SquareTerminal } from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "My Nest",
+      logo: GalleryVerticalEnd,
+      plan: "Apartment",
+    },
+  ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Apartments",
+          url: "/apartments",
+        },
+        {
+          title: "Owners",
+          url: "/owners",
+        },
+        {
+          title: "Tenants",
+          url: "/tenants",
+        },
+        {
+          title: "Payments",
+          url: "/payments",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
